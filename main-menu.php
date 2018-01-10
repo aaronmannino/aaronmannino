bd4ff816bf85e6f405769f89fcbd0d68863179dd
Aaron Mannino <aaronmannino@mac.com>
2018-01-10 03:35:29 -0500

    Less cutesy nav names.

--- main-menu.php
+++ main-menu.php
@@ -1,6 +1,6 @@
             <ul class="menu align-center main-menu">
-                <li class="<?php echo $selected_value_news; ?>"><a href="index.php">These Are News</a></li>
-                <li class="<?php echo $selected_value_music; ?>"><a href="music.php">This Is Music</a></li>
-                <li class="<?php echo $selected_value_shows; ?>"><a href="tour.php">These Are Show Dates</a></li>
-                <li class="<?php echo $selected_value_videos; ?>"><a href="videos.php">Here's Where There Are Videos</a></li>
+                <li class="<?php echo $selected_value_news; ?>"><a href="index.php">New</a></li>
+                <li class="<?php echo $selected_value_music; ?>"><a href="music.php">Music</a></li>
+                <li class="<?php echo $selected_value_shows; ?>"><a href="tour.php">Shows</a></li>
+                <li class="<?php echo $selected_value_videos; ?>"><a href="videos.php">Videos</a></li>
             </ul>
