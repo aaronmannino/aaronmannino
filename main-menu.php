976b8420ff4a9fed72f5126e3bd483d6dd9f776a
Aaron Mannino <aaronmannino@mac.com>
2018-01-10 03:37:21 -0500

    OMG. Always test locally first.

--- main-menu.php
+++ main-menu.php
@@ -1,5 +1,5 @@
             <ul class="menu align-center main-menu">
-                <li class="<?php echo $selected_value_news; ?>"><a href="index.php">New</a></li>
+                <li class="<?php echo $selected_value_news; ?>"><a href="index.php">News</a></li>
                 <li class="<?php echo $selected_value_music; ?>"><a href="music.php">Music</a></li>
                 <li class="<?php echo $selected_value_shows; ?>"><a href="tour.php">Shows</a></li>
                 <li class="<?php echo $selected_value_videos; ?>"><a href="videos.php">Videos</a></li>
